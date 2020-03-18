b35cf946eab254febfc18ef84bd094ef8d8971df
Kevin Moore <moore.kevin.p@gmail.com>
2020-03-18 14:46:01 -0400

    modularized error handler and added validate bearer token

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
 
 const app = express();
 
@@ -14,22 +16,16 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
+app.use(validateBearerToken);
+//put routers here
+
+//this needs to be the last piece of middleware
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
-});
-
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if(process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error'} };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
 });
 
 module.exports = app;
